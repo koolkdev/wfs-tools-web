c925d1d25c8bb9fbabd8919e372c2a77ad7be81e
koolkdev <koolkdev@gmail.com>
2025-03-09 23:56:44 +0200

    Remove pthread support

--- custom-triplets/wasm32-emscripten-pthread.cmake
+++
@@ -1,5 +0,0 @@
-# Include the base wasm32-emscripten triplet
-include("${VCPKG_ROOT_DIR}/triplets/community/wasm32-emscripten.cmake")
-
-# Set Emscripten flags for all packages
-set(ENV{EMCC_CFLAGS} "$ENV{EMCC_CFLAGS} -pthread")
