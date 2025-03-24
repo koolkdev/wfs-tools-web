0c11bdef3216077cf84f3e7438db6cca6ea59f24
koolkdev <koolkdev@gmail.com>
2025-03-24 21:23:50 +0200

    better

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -12,19 +12,19 @@ const AppLayout: React.FC = () => {
   return (
     <div className="flex flex-col min-h-[500px] h-screen max-h-screen">
       {/* Header */}
-      <header className="bg-primary-foreground px-4 py-2 text-primary shadow-md">
-        <div className="container flex items-center justify-between">
-          <div className="flex items-center gap-2">
+      <header className="bg-slate-200 dark:bg-slate-900 px-4 py-2 shadow-sm">
+        <div className="w-full flex items-center justify-between">
+          <div className="flex items-center">
             <img
               src={isDark ? wfsLogoDark : wfsLogoLight}
               alt="WFS Tools Logo"
-              className="h-10 w-10"
+              className="h-12 w-12"
             />
             <div className="flex items-baseline">
-              <h1 className="text-xl font-bold bg-gradient-to-r from-blue-200 to-blue-300 bg-clip-text text-transparent select-none">
+              <h1 className="text-xl font-bold bg-gradient-to-r from-blue-400 to-blue-500 bg-clip-text text-transparent select-none">
                 WFS
               </h1>
-              <span className="text-xl ml-1 text-blue-200 font-normal select-none">Explorer</span>
+              <span className="text-xl ml-1 text-blue-400 font-normal select-none">Explorer</span>
             </div>
           </div>
 
@@ -43,7 +43,7 @@ const AppLayout: React.FC = () => {
 
       {/* Footer */}
       <footer className="py-4 text-center border-t border-border">
-        <div className="container">
+        <div className="w-full flex justify-center">
           <p className="text-sm text-muted-foreground">© {new Date().getFullYear()} WFS Tools</p>
         </div>
       </footer>
